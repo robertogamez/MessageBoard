92e9554b6bcecccef37227d36d61f0425f6c814f
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-10-10 17:52:31 -0500

    Se añade modelo para el controlador de contacto

--- MessageBoard/Models/ContactModel.cs
+++ MessageBoard/Models/ContactModel.cs
@@ -2,5 +2,9 @@
 {
     public class ContactModel
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string Comment { get; set; }
     }
 }
